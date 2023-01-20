d6bf59be77943a12950d0d937332fdbcc2320bf0
Angelo Palomer <89753669+Artini04@users.noreply.github.com>
2023-01-20 00:11:36 -0700

    added styling

---
+++ README.md
@@ -0,0 +1,15 @@
+# kaibun-checker
+
+**Kaibun** (*Japanese:* 回文 or かいぶん, *lit.* 'circle sentence') is a Japanese equivalent of the palindrome, or in other words, a sentence that reads the same from the beginning to the end or from the end to the beginning. The unit of kaibun is mora, since the Japanese language uses syllabaries, hiragana and katakana. // https://en.wikipedia.org/wiki/Kaibun //
+
+---
+
+#### Usage
+
+1. Run the program
+
+```
+cargo run
+```
+
+2. Enter text
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use console::style;
 use kakasi::convert;
 use text_io::read;
 use wana_kana::ConvertJapanese;
 
 fn main() {
-    print!("Enter line: ");
+    println!(
+        "{} Text longer than 15 characters will be truncate.",
+        style("[NOTE]").yellow()
+    );
+    print!("{}", style("Enter text: ").bold());
     let line: String = read!("{}\n");
     kaibun_checker(line.as_str());
 }
@@ -11,16 +16,34 @@ fn main() {
 fn kaibun_checker(line: &str) -> bool {
     let line_string = convert(line).hiragana.to_hiragana();
     let line_rev = line_string.chars().rev().collect::<String>();
+    let line_len: usize = 15;
 
     match line_string == line_rev {
         true => {
-            println!("[/] {} : {}", line_string, line_rev);
+            println!(
+                "{} {} === {}",
+                style("[OK]").green(),
+                truncate(line_string.as_str(), line_len),
+                truncate(line_rev.as_str(), line_len)
+            );
             return true;
         }
         false => {
-            println!("[X] {} : {}", line_string, line_rev);
+            println!(
+                "{} {} =/= {}",
+                style("[NO]").red(),
+                truncate(line_string.as_str(), line_len),
+                truncate(line_rev.as_str(), line_len)
+            );
             return false;
         }
+    }
+}
+
+fn truncate(s: &str, max_char: usize) -> &str {
+    match s.char_indices().nth(max_char) {
+        None => s,
+        Some((idx, _)) => &s[..idx],
     }
 }
 
@@ -36,5 +59,5 @@ fn test_kaibun_checker() {
     assert!(!kaibun_checker("いせかい"));
     assert!(!kaibun_checker(
         "長き世の 遠の眠りの 皆目覚め 波乗り船の 音の良きかな"
-    )); // Phrases with half-width spaces will return false
+    )); // Phrases with spaces will return false
 }
